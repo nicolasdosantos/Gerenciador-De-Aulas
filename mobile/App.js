773be736be5711d84a39e69cee0c1edc3cc2f28e
Alexandre Milim <alexandremilim15@gmail.com>
2024-05-06 15:07:42 -0300

    Update App.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from './telas/Login';
+import Cadastro from './telas/Cadastro';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>MILIM up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login"
+                        component={Login}/>
+          <Stack.Screen name="Cadastro"
+                        component={Cadastro}/>
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
