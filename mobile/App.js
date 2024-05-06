bfa7f04c7420eb6b3d54953fac7d5cfa6fdd19e0
Alexandre Milim <alexandremilim15@gmail.com>
2024-05-06 15:13:37 -0300

    Update App.js

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>MURILO up App.js to start working on your app!</Text>
+      <Text>murilexxxsdsdsdsdsdssdsd up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
