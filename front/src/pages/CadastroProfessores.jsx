d1a1273b78b57e81a345e6b36bfdb3d46771e9ed
frithunanths476 <124930953+frithunanths476@users.noreply.github.com>
2024-05-14 15:28:39 -0300

    Update CadastroProfessores.jsx

--- front/src/pages/CadastroProfessores.jsx
+++ front/src/pages/CadastroProfessores.jsx
@@ -5,12 +5,119 @@ function CadastroProfessores() {
     return(
         <div className="App">
             <Banner>
-                <CardCadastro nome="Professores" 
-                              formNome="Nome: " 
-                              email="E-mail: " 
-                              senha="senha" cpf="CPF: " 
-                              formAdicional="NIF" 
-                              dataNascimento="Data de Nascimento"/>
+                <CardCadastro nome={"Professores"}>
+                    <label style={{
+                        fontWeight: "bold",
+                        marginTop: "15px",
+                        textAlign: "left",
+                        margin: "5px"
+                    }}>
+                        Nome:
+                    </label>
+                    <input type="text" style={{
+                        padding: "5px",
+                        width: "600px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        border: "none"
+                    }}/>
+
+                    <label style={{
+                        fontWeight: "bold",
+                        marginTop: "15px",
+                        textAlign: "left",
+                        margin: "5px"
+                    }}>
+                        E-mail:
+                    </label>
+                    <input type="text" style={{
+                        padding: "5px",
+                        width: "600px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        border: "none"
+                    }}/>
+
+                    <label style={{
+                        fontWeight: "bold",
+                        marginTop: "15px",
+                        textAlign: "left",
+                        margin: "5px"
+                    }}>
+                        Senha:
+                    </label>
+                    <input type="text" style={{
+                        padding: "5px",
+                        width: "600px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        border: "none"
+                    }}/>
+
+                    <label style={{
+                        fontWeight: "bold",
+                        marginTop: "15px",
+                        textAlign: "left",
+                        margin: "5px"
+                    }}>
+                        CPF:
+                    </label>
+                    <input type="text" style={{
+                        padding: "5px",
+                        width: "600px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        border: "none"
+                    }}/>
+
+                    <label style={{
+                        fontWeight: "bold",
+                        marginTop: "15px",
+                        textAlign: "left",
+                        margin: "5px"
+                    }}>
+                        CPF
+                    </label>
+                    <input type="number" style={{
+                        padding: "5px",
+                        width: "600px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        border: "none"
+                    }}/>
+
+                    <label style={{
+                        fontWeight: "bold",
+                        marginTop: "15px",
+                        textAlign: "left",
+                        margin: "5px"
+                    }}>
+                        NIF
+                    </label>
+                    <input type="number" style={{
+                        padding: "5px",
+                        width: "600px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        border: "none"
+                    }}/>
+
+                    <label style={{
+                        fontWeight: "bold",
+                        marginTop: "15px",
+                        textAlign: "left",
+                        margin: "5px"
+                    }}>
+                        Data de nascimento
+                    </label>
+                    <input type="date" style={{
+                        padding: "5px",
+                        width: "600px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        border: "none"
+                    }}
+                </CardCadastro>
             </Banner>
         </div>
     )
