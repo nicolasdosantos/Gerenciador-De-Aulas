4a0b9c3a1460d3c1b230a9e3b856dfbc7695900a
Alexandre Milim <124930757+Alexandre-Milim@users.noreply.github.com>
2024-05-27 15:20:30 -0300

    correção NavBar

--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -1,29 +1,21 @@
 import React from 'react';
 import css from './NavBar.module.css';
+
 function NavBar() {
     return (
         <div className={css.verticalNav}>
             <img className={css.logozinha} src="/logozinha.png" alt="Logo" />
-            <ul className={css.navbarNav} style={{listStyleType: 'none', padding: '0'}}>
-                <br/>
-                <br/>
+            <ul className={css.navbarNav} style={{ listStyleType: 'none', padding: '0' }}>
+                <br />
+                <br />
+                <li className={css.navItem}>
+                    <a className={css.navLink} href="/Home">Home</a>
+                </li>
                 <li className={css.navItem}>
                     <a className={css.navLink} href="/calendario">Calendário</a>
                 </li>
                 <li className={css.navItem}>
-                    <a className={css.navLink} href="/professores">Professores</a>
-                </li>
-                <li className={css.navItem}>
-                    <a className={css.navLink} href="/cursos">Cursos</a>
-                </li>
-                <li className={css.navItem}>
-                    <a className={css.navLink} href="/materias">Matérias</a>
-                </li>
-                <li className={css.navItem}>
-                    <a className={css.navLink} href="/salas">Salas</a>
-                </li>
-                <li className={css.navItem}>
-                    <a className={css.navLink} href="/alunos">Alunos</a>
+                    <a className={css.navLink} href="/cadastro">Cadastro</a>
                 </li>
                 <li className={css.navItem}>
                     <a id={css.sair} className={css.navLink} href="/inicial">Sair</a>
