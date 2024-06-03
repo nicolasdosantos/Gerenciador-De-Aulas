cae86a7c1bca9d9ccfe3771b549c575f243d9c7c
Alexandre Milim <124930757+Alexandre-Milim@users.noreply.github.com>
2024-06-03 11:16:20 -0300

    Update

--- front/src/components/NavBarPerfil.jsx
+++ front/src/components/NavBarPerfil.jsx
@@ -1,48 +1,58 @@
 import style5 from '../components/NavBarPerfil.module.css';
+import React, { useState } from 'react';
 
-function navBarPerfil({showPrograList}) {
+function NavBarPerfil({ showPrograList }) {
+    const [showPerfil, setShowPerfil] = useState(false);
+
+    const togglePerfil = () => {
+        setShowPerfil(!showPerfil);
+    };
 
     return (
         <div className={style5.principal}>
 
             <div className={style5.logoNav}>
-                <button><img src="/logoCoruja.png" alt="banner" className={style5.botaoImg}/>
+                <button onClick={togglePerfil}>
+                    <img src="/logoCoruja.png" alt="banner" className={style5.botaoImg}/>
                 </button>
             </div>
 
-            <div className={style5.principalPerfil}>
-                <div className={style5.perfil}>
-                    <img src="/perfil.png" alt="banner"
+            {showPerfil && (
+                <div className={style5.principalPerfil}>
+                    <div className={style5.perfil}>
+                        <img src="/perfil.png" alt="banner"
                              style={{width: '150px', height: '150px', borderRadius: '70px'}}/>
-                    <p className={style5.perfilNome}>Gustavo Donato</p>
-                    <p style={{color: '#8A8C98', marginBottom: '10px', fontSize: '18px'}}>Administrador</p>
+                        <p className={style5.perfilNome}>Gustavo Donato</p>
+                        <p style={{color: '#8A8C98', marginBottom: '10px', fontSize: '18px'}}>Administrador</p>
 
-                    <button className={style5.botao}><img src="/maisRoxo.png" alt="banner" className={style5.botaoImg}/>
-                        Criar
-                    </button>
-                </div>
+                        <button className={style5.botao}>
+                            <img src="/mais.png" alt="banner" className={style5.botaoImg}/>
+                            Criar
+                        </button>
+                    </div>
 
-                <h2 className={style5.tituloProgra}>Programação do dia:</h2>
-                <div className={style5.prograList} style={{ display: showPrograList ? 'block' : 'none' }}>
-                    <div className={style5.prograDiv}>
-                        <div className={style5.prograItemImage}>
-                            <img src="/sino.png" alt="sino" style={{ width: '50px', height: '50px' }} />
-                        </div>
+                    <h2 className={style5.tituloProgra}>Programação do dia:</h2>
+                    <div className={style5.prograList} style={{ display: showPrograList ? 'block' : 'none' }}>
+                        <div className={style5.prograDiv}>
+                            <div className={style5.prograItemImage}>
+                                <img src="/sino.png" alt="sino" style={{ width: '50px', height: '50px' }} />
+                            </div>
 
-                        <div className={style5.prograItem}>
-                            <p className={style5.prograTextT}>Desenvolvimento de sistemas</p>
-                            <p className={style5.prograTextC}>Python</p>
+                            <div className={style5.prograItem}>
+                                <p className={style5.prograTextT}>Desenvolvimento de sistemas</p>
+                                <p className={style5.prograTextC}>Python</p>
 
-                            <div className={style5.prograTextoDiv}>
-                                <p className={style5.prograTextS}>Sala 2</p>
-                                <p className={style5.prograTextH}>07:30</p>
+                                <div className={style5.prograTextoDiv}>
+                                    <p className={style5.prograTextS}>Sala 2</p>
+                                    <p className={style5.prograTextH}>07:30</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            )}
         </div>
     );
 }
 
-export default navBarPerfil;
+export default NavBarPerfil;
