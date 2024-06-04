f7d883b9f349da3a8f36a2ffa61ace4f7da5cef5
Alexandre Milim <alexandremilim15@gmail.com>
2024-06-04 08:09:17 -0300

    Update Dropdown.jsx

--- front/src/components/Dropdown.jsx
+++ front/src/components/Dropdown.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import css from './Dropdown.module.css';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
-import { FaTrashAlt, FaEdit,FaArrowUp } from 'react-icons/fa';
+import { FaTrashAlt, FaEdit, FaChevronDown, FaChevronUp } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 function Dropdown({ title, items, redirectPath }) {
@@ -29,19 +29,17 @@ function Dropdown({ title, items, redirectPath }) {
             <div className={css.content}>
                 <div className={css.dropdown}>
                     <button onClick={toggleDropdown} className={css.dropdownButton}>
-                        {title}
+                        <span className={css.dropdownTitle}>{title}</span>
+                        <span className={css.dropdownIcon}>{isDropdownOpen ? <FaChevronUp /> : <FaChevronDown />}</span>
                     </button>
                     {isDropdownOpen && (
                         <div className={css.dropdownContent}>
                             <div className={css.cardContainer}>
-                                <FaArrowUp/>
-
-
                                 {itemList.map((item, idx) => (
                                     <CardGroup key={idx}>
                                         <div className={css.editIcon}>
                                             <Link to={item.editButton}>
-                                                <FaEdit />
+                                                <FaEdit style={{ color: 'white' }} />
                                             </Link>
                                         </div>
                                         <Card className={css.cardizin}>
