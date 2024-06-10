62ca601b0d1c4dec3ae0e131eaf20321614d52d4
Alexandre Milim <124930757+Alexandre-Milim@users.noreply.github.com>
2024-06-10 11:01:50 -0300

    Update

--- front/src/components/Dropdown.jsx
+++ front/src/components/Dropdown.jsx
@@ -1,9 +1,11 @@
+// Dropdown.js
+
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import css from './Dropdown.module.css';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
-import { FaTrashAlt, FaEdit, FaChevronDown, FaChevronUp, FaSearch } from 'react-icons/fa';
+import { FaTrashAlt, FaEdit, FaChevronDown, FaChevronUp } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 function Dropdown({ title, items, redirectPath }) {
@@ -43,57 +45,50 @@ function Dropdown({ title, items, redirectPath }) {
                             <span className={css.dropdownIcon}>{isDropdownOpen ? <FaChevronUp /> : <FaChevronDown />}</span>
                         </button>
                     </div>
-                    {isDropdownOpen && (
-                        <div className={css.dropdownContent}>
-                            <div className={css.searchContainer}>
-                                <input
-                                    className={css.search}
-                                    type="text"
-                                    placeholder="| Pesquise o nome"
-                                    value={searchTerm}
-                                    onChange={handleSearchChange}
-                                />
-                            </div>
-                            <div className={css.cardContainer}>
-                                {filteredItems.map((item, idx) => (
-                                    <CardGroup key={idx}>
-                                        <div className={css.editIcon}>
-                                            <Link to={item.editButton}>
-                                                <FaEdit style={{color: 'white'}}/>
-                                            </Link>
-                                        </div>
-                                        <Card className={css.cardizin}>
-                                            <Card.Img variant="top" src={item.image} className={css.imgzinha}/>
-                                            <Card.Body>
-                                                <Card.Title id={css.nomePro}>{item.name}</Card.Title>
-                                                {item.nif && <Card.Text id={css.email}>NIF: {item.nif}</Card.Text>}
-                                                {item.turno &&
-                                                    <Card.Text id={css.email}>Turno: {item.turno}</Card.Text>}
-                                                {item.date &&
-                                                    <Card.Text id={css.email}>Dia do curso: {item.date}</Card.Text>}
-                                                {item.professor &&
-                                                    <Card.Text id={css.email}>Curso: {item.professor}</Card.Text>}
-                                                {item.description &&
-                                                    <Card.Text id={css.email}>Descrição: {item.description}</Card.Text>}
-                                                <div className={css.buttonContainer}>
-                                                    <button onClick={handleAddItem} className={css.addButton}>
-                                                        {item.buttin}
-                                                    </button>
-                                                    <button onClick={() => handleDeleteItem(idx)}
-                                                            className={css.deleteButton}>
-                                                        <FaTrashAlt/>
-                                                    </button>
-                                                </div>
-                                            </Card.Body>
-                                            <Card.Footer>
-                                                <small className={css.textmuted}>{item.email}</small>
-                                            </Card.Footer>
-                                        </Card>
-                                    </CardGroup>
-                                ))}
-                            </div>
+                    <div className={`${css.dropdownContent} ${isDropdownOpen ? css.show : ''}`}>
+                        <div className={css.searchContainer}>
+                            <input
+                                className={css.search}
+                                type="text"
+                                placeholder="| Pesquise o nome"
+                                value={searchTerm}
+                                onChange={handleSearchChange}
+                            />
                         </div>
-                    )}
+                        <div className={css.cardContainer}>
+                            {filteredItems.map((item, idx) => (
+                                <CardGroup key={idx}>
+                                    <div className={css.editIcon}>
+                                        <Link to={item.editButton}>
+                                            <FaEdit style={{ color: 'white' }} />
+                                        </Link>
+                                    </div>
+                                    <Card className={css.cardizin}>
+                                        <Card.Img variant="top" src={item.image} className={css.imgzinha} />
+                                        <Card.Body>
+                                            <Card.Title id={css.nomePro}>{item.name}</Card.Title>
+                                            {item.nif && <Card.Text id={css.email}>NIF: {item.nif}</Card.Text>}
+                                            {item.turno && <Card.Text id={css.email}>Turno: {item.turno}</Card.Text>}
+                                            {item.date && <Card.Text id={css.email}>Dia do curso: {item.date}</Card.Text>}
+                                            {item.professor && <Card.Text id={css.email}>Curso: {item.professor}</Card.Text>}
+                                            {item.description && <Card.Text id={css.email}>Descrição: {item.description}</Card.Text>}
+                                            <div className={css.buttonContainer}>
+                                                <button onClick={handleAddItem} className={css.addButton}>
+                                                    {item.buttin}
+                                                </button>
+                                                <button onClick={() => handleDeleteItem(idx)} className={css.deleteButton}>
+                                                    <FaTrashAlt />
+                                                </button>
+                                            </div>
+                                        </Card.Body>
+                                        <Card.Footer>
+                                            <small className={css.textmuted}>{item.email}</small>
+                                        </Card.Footer>
+                                    </Card>
+                                </CardGroup>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
